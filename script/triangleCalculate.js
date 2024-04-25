f485e4114fdb27c0e4e188a24541ca7d5f41cf0f
misskhat <misskhat.hossen@gmail.com>
2024-04-26 03:45:08 +0600

    rectangle area updated

--- script/triangleCalculate.js
+++ script/triangleCalculate.js
@@ -1,4 +1,4 @@
-function trangleCalcuate(){
+function trangleCalculate(){
     const triangleBaseInput = document.getElementById('triangle-base');
     const triangleBase = parseFloat(triangleBaseInput.value);
     const triangleHeightInput = document.getElementById('trangle-hight');
@@ -7,4 +7,15 @@ function trangleCalcuate(){
 
     const area = document.getElementById('area');
     area.innerText = triangleArea;
+}
+
+function rectangleCalculate() {
+    const rectangleWidthInput = document.getElementById('rectangle-width');
+    const rectangleWidth = parseFloat(rectangleWidthInput.value);  
+    const rectangleLenghtInput = document.getElementById('rectangle-lenght');
+    const rectangleLenght = parseFloat(rectangleLenghtInput.value);
+    const rectangleArea = rectangleWidth*rectangleLenght;
+    console.log(rectangleArea)
+    const area = document.getElementById('rectangle-area');
+    area.innerText = rectangleArea;
 }
