4e1ef022bc5772dfd73558000b77b7f4f9ff7e5d
misskhat <misskhat.hossen@gmail.com>
2024-04-28 02:47:59 +0600

    Parallelogram complete

--- script/triangleCalculate.js
+++ script/triangleCalculate.js
@@ -5,7 +5,7 @@ function trangleCalculate(){
     const triangleHeight = parseFloat(triangleHeightInput.value);
     const triangleArea = 0.5*triangleBase*triangleHeight;
 
-    const area = document.getElementById('area');
+    const area = document.getElementById('trangle-area');
     area.innerText = triangleArea;
 }
 
@@ -18,4 +18,18 @@ function rectangleCalculate() {
     console.log(rectangleArea)
     const area = document.getElementById('rectangle-area');
     area.innerText = rectangleArea;
+}
+
+function parallelogramCalculate(){
+    const parallelogramWidthInput = document.getElementById('parallelogram-width');
+    const parallelogramWidth = parseFloat(parallelogramWidthInput.value);
+    const parallelogramLenghtInput =document.getElementById('parallelogram-lenght');
+    const parallelogramLenght = parseFloat(parallelogramLenghtInput.value);
+    const area = parallelogramWidth * parallelogramLenght;
+    console.log(area); //result print in browser console//
+    // working for base and height show below the input
+    const base = document.getElementById('b-value');
+    const height = document.getElementById('h-value');
+    base.innerText = parallelogramWidth;
+    height.innerText = parallelogramLenght
 }
