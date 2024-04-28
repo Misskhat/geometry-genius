26f28f4bda5cccb63b759dcf7cb808bbefe3ecf2
misskhat <misskhat.hossen@gmail.com>
2024-04-29 03:51:57 +0600

    complete

--- script/triangleCalculate.js
+++ script/triangleCalculate.js
@@ -1,23 +1,3 @@
-function trangleCalculate(){
-    const triangleBaseInput = getInputElementById('triangle-base');
-    const triangleHeightInput = getInputElementById('trangle-hight');
-    const triangleArea = 0.5*triangleBaseInput*triangleHeightInput;
-    setValueAsInnerText('trangle-area', triangleArea);
-}
-
-function rectangleCalculate() {
-    const rectangleWidthInput = getInputElementById('rectangle-width');
-    const rectangleLenghtInput = getInputElementById('rectangle-lenght');
-    const rectangleArea = rectangleWidthInput*rectangleLenghtInput;
-    setValueAsInnerText('rectangle-area', rectangleArea)
-}
-
-function parallelogramCalculate(){
-    const parallelogramWidthInput = getInputElementById('parallelogram-width');
-    const parallelogramHeightInput = getInputElementById('parallelogram-lenght')
-    const parallelogramArea = parallelogramWidthInput * parallelogramHeightInput
-    setValueAsInnerText('parallegogram-area', parallelogramArea)
-}
 // reusable function start
 function getInputElementById(inputValue){
     const getInput = document.getElementById(inputValue);
@@ -26,7 +6,39 @@ function getInputElementById(inputValue){
 };
 function setValueAsInnerText(setValue, area){
     const setValueFrom = document.getElementById(setValue);
-    setValueFrom.innerText = area;
-    
+    setValueFrom.innerText = area;   
 }
 // reusable function end
+function trangleCalculate(){
+    const triangleBaseInput = getInputElementById('triangle-base');
+    const triangleHeightInput = getInputElementById('trangle-hight');
+    const triangleArea = 0.5*triangleBaseInput*triangleHeightInput;
+    setValueAsInnerText('trangle-area', triangleArea);
+};
+function rectangleCalculate() {
+    const rectangleWidthInput = getInputElementById('rectangle-width');
+    const rectangleLenghtInput = getInputElementById('rectangle-lenght');
+    const rectangleArea = rectangleWidthInput*rectangleLenghtInput;
+    setValueAsInnerText('rectangle-area', rectangleArea)
+};
+function parallelogramCalculate(){
+    const parallelogramWidthInput = getInputElementById('parallelogram-width');
+    const parallelogramHeightInput = getInputElementById('parallelogram-lenght')
+    const parallelogramArea = parallelogramWidthInput * parallelogramHeightInput
+    setValueAsInnerText('parallegogram-area', parallelogramArea)
+};
+
+function rhombusCalculate(){
+    const rhombusd1 = getInputElementById('rhombus-d1');
+    const rhombusd2 = getInputElementById('rhombus-d2');
+    const rhombusArea = 0.5 * rhombusd1 * rhombusd2;
+    setValueAsInnerText('rhombus-area', rhombusArea);
+}
+
+function pentagonCalculate(){
+    const pentagonPInput = getInputElementById('pentagonP');
+    const pentagonBInput = getInputElementById('pentagonB');
+    const pentagonArea = pentagonPInput * pentagonBInput;
+    setValueAsInnerText('pentagon-area', pentagonArea);
+}
+
